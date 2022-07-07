9fd2458aa54713cb1fcce85de0af57694dcdcf1d
afikri <fikri.abdul@gmx.com>
2022-07-07 16:01:06 +0700

    Added PMIRMP page

--- src/pages/training/PMIRMP.js
+++ src/pages/training/PMIRMP.js
@@ -2,7 +2,269 @@ import React from 'react'
 
 const PMIRMP = () => {
   return (
-    <div>PMIRMP</div>
+    <div>
+
+      <section id="partners" style={{
+        backgroundImage: "linear-gradient(110deg, #171F32 47%, #171F3229 100%)",
+        height: "60vh",
+        fontFamily: "Poppins",
+
+      }}>
+        <div className="container" >
+          <div className="col-lg-12" style={{
+            margin: "auto",
+            width: "100%",
+            padding: "10px",
+            color: "#FFF"
+          }}>
+
+            <h1 style={{ fontFamily: "Open Sans", }}>
+              <strong>
+                PMI-RMP® Certification Training Course
+              </strong>
+            </h1><br />
+            <div style={{ width: "800px" }}>
+              <div className="col-sm">
+                <div className="mt-5 d-flex align-items-center"
+                  style={{ justifyContent: "space-between" }}>
+                  PMI Risk Management Professional (PMI-RMP®)
+                  <div className="small-ratings" style={{ alignContent: "right" }}>
+                    <i className="fa fa-star rating-color"></i>
+                    <i className="fa fa-star rating-color"></i>
+                    <i className="fa fa-star rating-color"></i>
+                    <i className="fa fa-star rating-color"></i>
+                    <i className="fa fa-star rating-color"></i>
+                  </div>&nbsp;
+                  <div>
+                    4148 Ratings&nbsp; |
+                    17990 Learners
+                  </div>
+                </div>
+              </div>
+              <br />
+              <div style={{ textAlign: "justify" }}>
+                <p>
+                  PMI-RMP or Project Risk Management Professional is issued by Project Management Institute (PMI), the world’s leading not-for-profit professional membership association for the project, program and portfolio management profession.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="certifications">
+        <div className="container">
+          <div className="section-header">
+            <h4 style={{
+              fontFamily: "Montserrat",
+              fontSize: "24px",
+              color: "#54595F",
+              textTransform: "uppercase",
+              fontWeight: "700",
+              position: "relative",
+              paddingBottom: "20px"
+            }}>Courses and Overview</h4>
+            <div className="col-md-12 content" >
+              <div className="col-sm" style={{ paddingBottom: "10px" }}>
+                <div style={{ textAlign: "justify", display: 'grid', gridTemplateColumns: "100%" }}>
+                  <p>
+                    PMI-RMP Credential is widely recognized by the industry as the standard for Project Risk Management qualification. In this course you will learn about how to manage project risks, mitigate threats and capitalize on opportunities according to best practice and make your project successful. In this capacity, you enhance and protect the needs of your organization.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </section >
+
+      <section id="certifications">
+        <div className="container">
+          <div className="section-header">
+            <h2 style={{
+              fontFamily: "Montserrat",
+              fontSize: "24px",
+              color: "#54595F",
+              // textTransform: "uppercase",
+              fontWeight: "700",
+              position: "relative",
+              paddingBottom: "20px"
+            }}>PMI-RMP® Training</h2>
+
+            <div className="col-lg-7 content" style={{ textAlign: "justify" }}>
+              <p>
+                Our PMI-RMP training helps you to use the best risk management principles to execute projects successfully. You’ll learn to plan for each project with a structured approach that is unique to that project and learn to keep your team focused throughout the lifetime of the project. You’ll also get an introduction into preparing for and taking the risk management examination, and qualify for 30 contact hours of training.
+              </p>
+              <div className="col-sm" style={{ paddingBottom: "10px" }}>
+                <h4 style={{ fontFamily: 'Poppins', fontSize: '18px' }}><b>Training Syllabus:</b></h4>
+                <div style={{ display: 'grid', gridTemplateColumns: "50% 50%" }}>
+                  <div>
+                    <ul style={{ listStyle: "none" }}>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Risk Management Introduction</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Risk Principles and Concepts</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Plan Risk Management</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Identify Risk</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Perform Qualitative Risk Analysis</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Perform Quantitative Risk Analysis</li>
+
+                    </ul>
+                  </div>
+                  <div>
+                    <ul style={{ listStyle: "none" }}>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Perform Quantitative Risk Analysis</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Plan Risk Response</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Monitor and Control Project Risk</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>PMI-RMP Tips and Tricks</li>
+                      <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>PMI-RMP Simulation Exam</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </section >
+
+      <section id="certifications">
+        <div className="container">
+          <div className="section-header">
+            <h2 style={{
+              fontFamily: "Montserrat",
+              fontSize: "24px",
+              color: "#54595F",
+              fontWeight: "700",
+              position: "relative",
+              paddingBottom: "20px"
+            }}>PMI-RMP® Mentoring</h2>
+            <div className="col-lg-12 content" style={{ textAlign: "justify" }}>
+              <p>
+                Our PMI-RMP Mentoring is an activity provided for participants who are preparing to proceed to certification (exams) with a 2-4 hours session meeting, the trainer will help participants develop more effectively with additional knowledge, skills and advice not available in the training. In Mentoring and Coaching activities, participants are required to do the tasks given by the trainer every week and attend the meeting once a week for 7 sessions.
+              </p>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </section >
+
+
+      <section id="certifications">
+        <div className="container">
+          <div className="section-header">
+            <h2 style={{
+              fontFamily: "Montserrat",
+              fontSize: "24px",
+              color: "#54595F",
+              fontWeight: "700",
+              position: "relative",
+              paddingBottom: "20px"
+            }}>PMI-RMP® Exam</h2>
+            <div className="col-lg-12 content" style={{ textAlign: "justify" }}>
+              <p>
+                Our PMI-RMP exam is part of us to help participants fill out the exam application, and the data needed to get the eligibility code as one of the PMI-RMP exam requirements. If participants are exposed to random audit enetwoq will be charged a fee, participants will also get a full 6 months PMI-RMPExam simulation.
+              </p>
+            </div>
+            <div className="col-sm" style={{ paddingBottom: "10px" }}>
+              <h4 style={{ fontFamily: 'Poppins', fontSize: '18px' }}><b>Pre-requisites:</b></h4>
+              <p>PMI-RMP Exam Requirements:</p>
+              <div style={{ display: 'grid', gridTemplateColumns: "50% 50%" }}>
+                <div>
+                  <ul style={{ listStyle: "none" }}>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Secondary degree (high school diploma, associate’s degree, or the global equivalent)</li>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>36 months of project risk management experience within the last 5 years</li>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>40 hours of project risk management education</li>
+
+                    <li>
+                      <i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }}></i>
+                      <b>OR</b>
+                    </li>
+
+
+                  </ul>
+                </div>
+                <div>
+                  <ul style={{ listStyle: "none" }}>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>Four-year degree (bachelor’s degree or the global equivalent)</li>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>24 months of project risk management experience within the last 5 years</li>
+                    <li><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>30 hours of project risk management education</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm" style={{ paddingBottom: "10px" }}>
+              <h4 style={{ fontFamily: 'Poppins', fontSize: '18px' }}><b>Facilitator:</b></h4>
+              <p>Project and Risk management practitioner with over 7 years of experience in industry and holding valid PMI-RMP certification.</p>
+            </div>
+            <div>
+            <h4 style={{ fontFamily: 'Poppins', fontSize: '18px', paddingBottom: "15px" }}><b>Our PMI-RMP® Products</b></h4>
+            </div>
+
+            <div className="row gy-4">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="card mb-3 h-100">
+                    <div className="card-header text-white" style={{ background: "#54595F", height: "110px", textAlign: 'center' }}>
+                      <h4>PRINCE2® Foundation Full Package</h4>
+                      <div>Training + Exam</div>
+                    </div>
+                    <div className="card-body" style={{ textAlign: "justify", background: "#F7F7F7" }}>
+                      <ul style={{ listStyle: "none" }}>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Include Exam</span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Online by Zoom</span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Module Training in Softcopy </span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Supporting Document in Softcopy </span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Life time consultacy after training (WAG) </span>
+                        </li>
+
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "15px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">PRINCE2® Foundation Simulation </span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="card mb-3 h-100">
+                    <div className="card-header text-white" style={{ background: "#54595F", height: "110px", textAlign: 'center' }}>
+                      <h4>PRINCE2® Foundation Corporate Training</h4>
+                      <div>Customized to your team’s need</div>
+                    </div>
+                    <div className="card-body" style={{ textAlign: "justify", background: "#F7F7F7" }}>
+                      <ul style={{ listStyle: "none" }}>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Customized Online/Offline</span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Module Training in Softcopy</span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Supporting Document in Softcopy</span>
+                        </li>
+                        <li style={{ borderBottom: "2px dotted #54595F" }}><i style={{ fontSize: "20px", paddingRight: "4px", color: "#50d8af", fontWeight: "400", }} className="bi bi-check-circle"></i>
+                          <span className="list-name">Life time consultacy after training (WAG)</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section >
+
+    </div >
   )
 }
 
