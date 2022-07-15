c0d9852bc2bf795841e8242c9c520c697c6dfaae
afikri <fikri.abdul@gmx.com>
2022-07-15 08:40:36 +0700

    Added services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,219 @@
 import React from 'react'
+import service from '../assets/service_cropped.png';
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <div>
+        <img src={service} style={{ backgroundPosition: "center", backgroundSize: "cover", margin: "0", height: "80%", width: "100%" }} alt="logo" />
+      </div>
+      <section id="certifications">
+        <div className="container">
+          <div className="row gy-4">
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    Digital Transformation
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    Scrum & Agile Enterprise
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="container" style={{ marginTop: "30px" }}>
+          <div className="row gy-4">
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    Project Management Consulting
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    PMO Consulting
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container" style={{ marginTop: "30px" }}>
+          <div className="row gy-4">
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    IT Service Management Consulting
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100" style={{ backgroundColor: "#FCFCFC" }}>
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    IT Security Management Consulting
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h3 className="heading">Project Management Professional</h3>
+                  <div className="mt-2">
+                    <div className="row">
+                      <div className="col-sm">
+                        <div className="mt-5 d-flex align-items-center">
+                          <h5 className="review-stat">4.5 &nbsp;</h5>
+                          <div className="small-ratings">
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star rating-color"></i>
+                            <i className="fa fa-star-half rating-color"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        80343 Learners
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+        </div>
+      </section>
+
+    </div>
   )
 }
 
