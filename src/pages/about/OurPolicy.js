d4db52208db59a88401cf868d7e71bb16d0c201d
afikri <fikri.abdul@gmx.com>
2022-07-21 21:06:29 +0700

    Completed our policy  page

--- src/pages/about/OurPolicy.js
+++ src/pages/about/OurPolicy.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import privacy from '../../assets/Privacy.png'
+import privacy2 from '../../assets/Privacy2.png'
 
 const OurPolicy = () => {
   return (
-    <div>OurPolicy</div>
+    <div>
+      <div>
+        <img src={privacy} style={{ backgroundPosition: "center", backgroundSize: "cover", margin: "0", height: "80%", width: "100%" }} alt="logo" />
+      </div>
+      <div>
+        <div class="timeline">
+          <div class="my-container">
+            <div class="content" style={{ backgroundColor: "#F0F0F0" }}>
+              <h2>2021</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
