71582d655d21e538ae7a9b1c18f465f1947abb95
afikri <fikri.abdul@gmx.com>
2022-07-25 14:11:03 +0700

    Completed contact us page

--- src/pages/about/ContactUs.js
+++ src/pages/about/ContactUs.js
@@ -1,8 +1,196 @@
 import React from 'react'
+import contactus from '../../assets/contactus.png'
+
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <div style={{ backgroundColor: "#686868" }}>
+        <img src={contactus} style={{
+          backgroundSize: "cover",
+          margin: "0",
+          width: "auto",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }} alt="logo" />
+      </div>
+      <section id="certifications">
+        <div className="container">
+          <div className="section-header">
+            <h4 style={{
+              fontFamily: "Montserrat",
+              fontSize: "24px",
+              color: "#54595F",
+              textTransform: "uppercase",
+              fontWeight: "700",
+              position: "relative",
+              paddingBottom: "20px"
+            }}>Contact Us</h4>
+            <h6
+              style={{
+                fontFamily: "Montserrat",
+                fontSize: "18px",
+                color: "#54595F",
+                textTransform: "uppercase",
+                fontWeight: "700",
+                position: "relative",
+                textAlign: "center",
+                verticalAlign: "middle",
+                paddingBottom: "20px"
+              }}
+            >Our location<br />Indonesia</h6>
+          </div>
+        </div>
+      </section >
+      <section id="certifications">
+        <div className="container">
+          <div className="row gy-4">
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100">
+                <div className="mt-5">
+                  <h6
+                    style={{
+                      fontFamily: "Montserrat",
+                      fontSize: "26px",
+                      color: "#54595F",
+                      textTransform: "uppercase",
+                      fontWeight: "700",
+                      position: "relative",
+                      textAlign: "center",
+                      verticalAlign: "middle",
+                      paddingBottom: "20px"
+                    }}
+                  >Jakarta</h6>
+                  <i className="fa fa-map-marker fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                  <h5>
+                    South Quarter Tower A
+                    18th Floor Jl. R.A Kartini Kav 8, Cilandak Barat, Jakarta, 12430
+                  </h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="card p-3 mb-2 h-100">
+                <div className="mt-5">
+                  <div className="ms-2 c-details">
+                    <h6
+                      style={{
+                        fontFamily: "Montserrat",
+                        fontSize: "26px",
+                        color: "#54595F",
+                        textTransform: "uppercase",
+                        fontWeight: "700",
+                        position: "relative",
+                        textAlign: "center",
+                        verticalAlign: "middle",
+                        paddingBottom: "20px"
+                      }}
+                    >Depok</h6>
+                  </div>
+                  <i className="fa fa-map-marker fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                  <h5>
+                    Telaga Golf
+                    Business Center Milan Avenue Blok R-CE No.19, Sawangan, Depok, 16511
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact">
+        <div className="container">
+          <div className="row contact-info">
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Contact Form</h3>
+                <i className="fa fa-wpforms fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Email Info</h3>
+                <i className="fa fa-envelope fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Email Training</h3>
+                <i className="fa fa-line-chart fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                <p><a href="tel:+155895548855">+1 5589 55488 55</a></p>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Telephone</h3>
+                <i className="fa fa-phone fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                <p><a href="tel:+155895548855">+1 5589 55488 55</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="container">
+          <div className="row contact-info">
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Mobile Phone</h3>
+                <i className="fa fa-mobile fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Live Chat</h3>
+                <i className="fa fa-comments fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+
+                <h3>Whatsapp</h3>
+                <i className="fa fa-whatsapp fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                <p><a href="tel:+155895548855">+1 5589 55488 55</a></p>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div style={{
+                textAlign: "center"
+              }}>
+                <h3>Telegram</h3>
+                <i className="fa fa-telegram fa-4x" style={{ marginBottom: "15px", display: "flex", justifyContent: "center", color: "#2575FC" }}></i>
+                <p><a href="mailto:info@example.com">info@example.com</a></p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </section>
+    </div>
   )
 }
 
